1be883331751bb634d8b29ec8c948061853232f8
Tarik Jeait <69420323+tarik-jeait@users.noreply.github.com>
2026-08-22 19:07:16 +0100

    0.1.1

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 //% color=190 weight=100 icon="\uf1ec" block="MCarBotBasic"
 //% groups=['General','RGB LED', 'Line Follower', 'Moves']
 namespace MCarBotBasic {
-
     export enum Dir {
         Forward = 0,
         Backward = 1
-    }
+    };
+    export enum Led {
+        Right = 0,
+        Left = 1,
+        All = 2
+    };
     //% blockId=mcarbot_init
     //% block="McarBot Init"
     //% group="General"
@@ -17,46 +21,19 @@ namespace MCarBotBasic {
     export function McarBotReset() {
         sendI2cWriteCommand(2, 1, 0, 0, 0, 0, 0, 0);
     }
-    //% block='Left Led R:$red G:$green B:$blue'
-    //% red.defl=255
-    //% red.min=0 red.max=255
-    //% green.defl=255
-    //% green.min=0 green.max=255
-    //% blue.defl=255
-    //% blue.min=0 blue.max=255
-    //% group='RGB LED'
-    export function LeftLed(red: number, green: number, blue: number) {
-        sendI2cWriteCommand(11, 1, red, green, blue, 0, 0, 0);
-    }
-
-    //% block='RLed R:$rred G:$rgreen B:$rblue|LLed R:$lred G:$lgreen B:$lblue'
+    //% block='LED:$led R:$red G:$green B:$blue'
     //% inlineInputMode=inline
-    //% rred.defl=255
-    //% rred.min=0 rred.max=255
-    //% rgreen.defl=255
-    //% rgreen.min=0 rgreen.max=255
-    //% rblue.defl=255
-    //% rblue.min=0 rblue.max=255
-    //% lred.defl=255
-    //% lred.min=0 lred.max=255
-    //% lgreen.defl=255
-    //% lgreen.min=0 lgreen.max=255
-    //% lblue.defl=255
-    //% lblue.min=0 lblue.max=255    
+    //% red.defl=1
+    //% red.min=0 red.max=1
+    //% green.defl=1
+    //% green.min=0 green.max=1
+    //% blue.defl=1
+    //% blue.min=0 blue.max=1
     //% group='RGB LED'
-    export function BothLeds(rred: number, rgreen: number, rblue: number, lred: number, lgreen: number, lblue: number) {
-        sendI2cWriteCommand(13, 1, rred, rgreen, rblue, lred, lgreen, lblue);
-    }
-    //% block='Right Led R:$red G:$green B:$blue'
-    //% red.defl=255
-    //% red.min=0 red.max=255
-    //% green.defl=255
-    //% green.min=0 green.max=255
-    //% blue.defl=255
-    //% blue.min=0 blue.max=255
-    //% group='RGB LED'
-    export function Rightled(red: number, green: number, blue: number) {
-        sendI2cWriteCommand(12, 1, red, green, blue, 0, 0, 0);
+    export function setLed(led: Led, red: number, green: number, blue: number) {
+        i2cWriteCommand(10, led, red, green, blue, 0);
+        basic.pause(50);
+        i2cReadCommand();
     }
 
     //% block = "Line Follower Sensors On"
@@ -95,6 +72,19 @@ namespace MCarBotBasic {
     export function getRpAddress() {
         let rp_address = 0x13;
         return rp_address;
+    }
+    function i2cWriteCommand(commandId: number, subCommandId: number, arg1: number, arg2: number, arg3: number, arg4: number) {
+        let writeCommandBuffer = pins.createBuffer(6);
+        writeCommandBuffer.setNumber(NumberFormat.UInt8LE, 0, commandId);
+        writeCommandBuffer.setNumber(NumberFormat.UInt8LE, 1, subCommandId);
+        writeCommandBuffer.setNumber(NumberFormat.UInt8LE, 2, arg1);
+        writeCommandBuffer.setNumber(NumberFormat.UInt8LE, 3, arg2);
+        writeCommandBuffer.setNumber(NumberFormat.UInt8LE, 4, arg3);
+        writeCommandBuffer.setNumber(NumberFormat.UInt8LE, 5, arg4);
+        pins.i2cWriteBuffer(getRpAddress(), writeCommandBuffer, false);
+    }
+    function i2cReadCommand() {
+        return pins.i2cReadBuffer(getRpAddress(), 6, false);
     }
     function sendI2cWriteCommand(commandId: number, commandType: number, arg1: number, arg2: number, arg3: number, arg4: number, arg5: number, arg6: number) {
         let writeCommandBuffer = pins.createBuffer(8);
